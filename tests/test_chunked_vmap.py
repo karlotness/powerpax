04b61db1f3226a14ee69ee74256fea9bcffd3cad
Karl Otness <karl@karlotness.com>
2024-02-15 01:10:47 -0500

    Add additional tests for chunked_vmap.
    Make sure chunk_vmap works if either of args or kwargs is empty.

--- tests/test_chunked_vmap.py
+++ tests/test_chunked_vmap.py
@@ -9,13 +9,18 @@ import pytest
 
 
 @pytest.mark.parametrize("chunk_size", [1, 3, 5, 9, 10, 11])
-def test_matches_vmap(chunk_size):
-    def fun(arg):
-        return arg * 2
+@pytest.mark.parametrize(
+    "use_args,use_kwargs", [(True, True), (True, False), (False, True)]
+)
+def test_matches_vmap(chunk_size, use_args, use_kwargs):
+    def fun(arg=1, arg2=1):
+        return arg * arg2
 
     arr = jnp.arange(10)
-    jax_res = jax.vmap(fun)(arr)
-    chunked_res = jax.jit(ppx.chunked_vmap(fun, chunk_size=chunk_size))(arr)
+    args = (arr,) if use_args else ()
+    kwargs = {"arg2": arr} if use_kwargs else {}
+    jax_res = jax.vmap(fun)(*args, **kwargs)
+    chunked_res = jax.jit(ppx.chunked_vmap(fun, chunk_size=chunk_size))(*args, **kwargs)
     assert jax_res.dtype == chunked_res.dtype
     assert jax_res.shape == chunked_res.shape
     assert jnp.all(jax_res == chunked_res)
