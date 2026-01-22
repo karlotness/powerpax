654849bd9878077199db2c8bce596e9e1299ae41
Karl Otness <karl@karlotness.com>
2026-01-22 11:44:02 -0500

    Add functional test for Static operation under JIT.

--- tests/test_static.py
+++ tests/test_static.py
@@ -14,6 +14,15 @@ def test_roundtrip_static():
     assert hash(obj) == hash(obj2)
 
 
+def test_static_under_jit():
+    @jax.jit
+    def f(arg1, arg2):
+        return arg1 * ord(arg2.value)
+
+    assert f(2, ppx.Static("a")) == 2 * ord("a")
+    assert f(3, ppx.Static("b")) == 3 * ord("b")
+
+
 def test_no_leaves():
     obj = ppx.Static(1.5)
     leaves = jax.tree_util.tree_leaves(obj)
